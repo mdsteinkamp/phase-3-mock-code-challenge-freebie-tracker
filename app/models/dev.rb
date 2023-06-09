8d002cdae198e49a69904e60a0ec0cf9af31a465
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-08 18:42:22 -0700

    commit tree 15b28d565d8650b76750b96b346ab92373ddf26e

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.map{ |freebie| freebie.item_name}.include?(item_name)
+  end
+
+  def give_away(dev, freebie)
+    if freebie.dev == self
+      freebie.dev = dev
+      freebie.save
+    else puts "Sorry that dev cannot give that freebie away!"
+    end
+  end
 
 end
